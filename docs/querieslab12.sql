da26d08a3dccfd060490192b38419ede0d892269
Alex <68801468+acastilloi21@users.noreply.github.com>
2024-11-25 00:35:54 -0500

    committed queries up to 8

--- docs/querieslab12.sql
+++ docs/querieslab12.sql
@@ -13,3 +13,26 @@ FROM assignments
 WHERE due_date >date()
 
 --4 Count Assignments by Status:
+SELECT id entry, status, COUNT (*)
+FROM assignments
+GROUP by status
+
+--5 Longest Course Name:
+SELECT course_name
+from courses
+order by length(course_name) DESC
+LIMIT 1
+
+--6 Uppercase Course Names:
+SELECT upper(course_name)
+FROM courses
+
+--7 Assignments Due in September:
+SELECT *
+FROM assignments
+where due_date LIKE '%-09-%'
+
+--8 Assignments with Missing Due Dates:
+SELECT *
+FROM assignments
+where due_date is NULL
