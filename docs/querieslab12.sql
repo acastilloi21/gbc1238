95d33d8ac9a12e73030aae0646f180af7ba7f0e4
Alex <68801468+acastilloi21@users.noreply.github.com>
2024-11-25 01:15:26 -0500

    added Extra task queries from lab12

--- docs/querieslab12.sql
+++ docs/querieslab12.sql
@@ -36,3 +36,28 @@ where due_date LIKE '%-09-%'
 SELECT *
 FROM assignments
 where due_date is NULL
+
+--E1 Total Assignments per Course:
+-- teacher you may not have been here but there was a victory warcry after this ran as intended
+SELECT courses.course_id, title, status,due_date, COUNT(*) 'No of Assignments'
+from assignments
+LEFT JOIN courses WHERE assignments.course_id = courses.course_id
+GROUP BY course_name
+
+--E2 Courses Without Assignments (advanced):
+-- Approaches
+SELECT courses.course_id, course_name, title, status, due_date, COUNT(*) 'No of Assignments'
+from assignments
+LEFT JOIN courses WHERE assignments.id is NULL
+GROUP by courses.course_id
+
+-- Attempt 2
+SELECT courses.course_id, title, status,due_date, COUNT(*) 'No of Assignments'
+from assignments
+LEFT JOIN courses WHERE assignments.course_id = courses.course_id
+GROUP BY courses.course_id
+AND assignments.id is NULL
+
+-- Query rendered unfruitful, below was run to insert new course yet still did not appear in above queries.
+INSERT INTO courses (course_id, semester, course_name) 
+VALUES ('COMP7357','2024-2','TEST NO ASSIGNMENT')
